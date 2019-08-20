27ee24cf332062b29edc6ba6905823805e3071dd
Wilton Gonçalves <wilton.sgoncalves@gmail.com>
2019-08-19 21:07:19 -0300

    Segundo botão back to top

--- public/js/back-to-top.js
+++ public/js/back-to-top.js
@@ -1,27 +1,16 @@
-$(document).ready(function() {
-    // plugin initializations etc.
-    $(".button-collapse").sideNav();
-  });
-  
-  if ($('#back-to-top').length) {
-    var scrollTrigger = 100, // px
-      backToTop = function() {
-        var scrollTop = $(window).scrollTop();
-        if (scrollTop > scrollTrigger) {
-          $('#back-to-top').removeClass('scale-out');
-        } else {
-          $('#back-to-top').addClass('scale-out');
-        }
-      };
-    backToTop();
-    $(window).on('scroll', function() {
-      backToTop();
-    });
-    
-    $('#back-to-top').on('click', function(e) {
-      e.preventDefault();
-      $('html,body').animate({
-        scrollTop: 0
-      }, 700);
-    });
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
   }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
